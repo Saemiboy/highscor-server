22943c0fe58b8c6e0fbfb9a5ce12fd6a927bec4a
Samuel Egli <samuelegligarcia@outlook.de>
2025-02-25 14:06:14 +0100

    Server fertig gebaut

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const FILE_PATH = "./highscore.json";
+
+
+// Highscore aus datei laden
+let highscore = 0;
+if (fs.existsSync(FILE_PATH)) {
+    const data = fs.readFileSync(FILE_PATH);
+    highscore = JSON.parse(data).highscore;
+}
+
+// Endpunkt API zum abrufen des Highscores
+app.get("/get_highscore", (req, res) => {
+    res.json({highscore});
+});
+
+
+// Endpunkt API zum setzten des Highscores
+app.post("/set_highscore", (req, res) => {
+    const newScore = req.body.score;
+    if (newScore > highscore) {
+        highscore = newScore;
+        fs.writeFileSync(FILE_PATH, JSON.stringify({highscore}));
+    }
+    res.json({highscore})
+});
+
+
+// Server Starten
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}.`)
+})
